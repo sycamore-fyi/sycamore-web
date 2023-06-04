7b97249d541a58327b9c7062f6d46ad788519b8b
Matthew Ffrench <matt.e.ffrench@gmail.com>
2023-06-04 21:51:02 +0100

    replace all forms with formutils

--- frontend/src/routes/organisations/create.tsx
+++ frontend/src/routes/organisations/create.tsx
@@ -1,65 +1,35 @@
-import { Button } from "@/components/ui/button"
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { postServer } from "@/lib/callServer"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Environment } from "@sycamore-fyi/shared"
-import { useForm } from "react-hook-form"
 import { useNavigate } from "react-router-dom"
 import { z } from "zod"
-
-const createOrgFormSchema = z.object({
-  name: z.string()
-})
-
-type CreateOrgFormData = z.infer<typeof createOrgFormSchema>
+import { FormUtil } from "../auth/FormUtil"
+import { FormFieldUtil } from "../auth/FormFieldUtil"
 
 export default function CreateOrganisationPage() {
-  const emailLinkForm = useForm<CreateOrgFormData>({
-    resolver: zodResolver(createOrgFormSchema),
-    defaultValues: {
-      name: "",
-    },
-  })
-
-  console.log(Environment)
-
-
   const navigate = useNavigate()
-
-  const onSubmitCreateOrgForm = async ({ name }: CreateOrgFormData) => {
-    const res = await postServer("/organisations", {
-      name
-    })
-
-    const { organisationId } = res.data
-
-    navigate(`/organisations/${organisationId}`)
-  }
 
   return (
     <div>
       Create organisation
-      <Form {...emailLinkForm}>
-        <form onSubmit={emailLinkForm.handleSubmit(onSubmitCreateOrgForm)} className="space-y-4">
-          <FormField
-            control={emailLinkForm.control}
+      <FormUtil
+        schema={z.object({
+          name: z.string()
+        })}
+        defaultValues={{ name: "" }}
+        submitTitle="Create organisation"
+        onSubmit={async ({ name }) => {
+          const res = await postServer("/organisations", { name })
+          const { organisationId } = res.data
+          navigate(`/organisations/${organisationId}`)
+        }}
+        render={form => (
+          <FormFieldUtil
+            control={form.control}
             name="name"
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>Name</FormLabel>
-                <FormControl>
-                  <Input {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
+            render={({ field }) => <Input {...field} />}
           />
-          <Button className="w-full" type="submit">
-            Create organisation
-          </Button>
-        </form>
-      </Form>
+        )}
+      />
     </div>
   )
 }
